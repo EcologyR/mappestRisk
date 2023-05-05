3322e6ccc9507c796d950ac554e25a0d5a27c148
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2023-05-05 15:31:27 +0200

    revise plot_map

--- R/plot_map.R
+++ R/plot_map.R
@@ -1,53 +1,65 @@
 #' Generate an interactive map from a risk map
 #'
-#' @param x a raster object
-#' @param map_type the type of map. One of "high" or "medium".
+#' @param x a SpatRaster object
+#' @param map_type the type of map. One of "high" or "medium" risk.
 #' @param legend_title the title of the legend.
-#' @param path_out the name and the path of the output file. Example: "data/map.html"
+#' @param path_out (optional) the name and the path of the output file. Example: "data/map.html"
 #' @param pal the palette used to plot the risk map. Default ...
+#' @param ... further arguments (CURRENTLY NOT USED)
 #'
-#' @return a dynamic map and a html with the dynamic map
+#' @return a dynamic map and, optionally, a html with the dynamic map
 #' @export
 #'
 #' @example
 #' \dontrun{
-#' p <- tempdir()
-#' r <- raster::raster("inst/trioza_rast.tif")
+#' r <- system.file("extdata", "trioza_rast.tif", package = "mappestRisk")
+#' # r <- terra::rast("inst/extdata/trioza_rast.tif")
 #' plot_map(x = r, map_type = "high", path_out = paste0(tempdir(), "test_map.html"))
 #' }
 #'
-plot_map <- function(x, legend_title = NULL,
-                     map_type = "high",
+plot_map <- function(x,
+                     legend_title = NULL,
+                     map_type = c("high", "medium"),
                      path_out = NULL,
-                     pal = NULL, ...){
+                     pal = NULL,
+                     ...) {
 
   #  if(!is(x, "RasterLayer"))
   # if(!any(x %in% c("SpatRaster", "RasterLayer", "stars", "terra")))
   #     stop("x has to be a raster object")
 
+  stopifnot(inherits(x, "SpatRaster") | inherits(x, "RasterLayer"))
+
+  # current leaflet version on CRAN (2.1.2) does not seem to accept SpatRaster yet
+  # temporary fix:
+  if (inherits(x, "SpatRaster")) {
+    x <- raster::raster(x)
+  }
+
   # argument legend
   if (is.null(legend_title)) {
-    map_type <- match.arg(map_type, c("high", "medium"))
+    map_type <- match.arg(map_type)
     legend_title <- paste0("Number of ", map_type, " suitable months")
   }
 
   # palette
   if (is.null(pal)) {
     mycolors <- c("grey65",
-                  colorRampPalette(c("#0081a7","#00afb9","#fdfcdc","#fed9b7","#f07167"))(12))
-    pal <- colorFactor(mycolors, values(x), na.color = "transparent")
+                  grDevices::colorRampPalette(c("#0081a7","#00afb9","#fdfcdc","#fed9b7","#f07167"))(12)
+                  )
+    pal <- leaflet::colorFactor(mycolors, values(x), na.color = "transparent")
   }
 
-  outmap <- leaflet() |>
-    addProviderTiles(providers$CartoDB.Positron,
+  outmap <- leaflet::leaflet() |>
+    leaflet::addProviderTiles(leaflet::providers$CartoDB.Positron,
                      group = "Basemap") |>
-    addProviderTiles(providers$Esri.WorldImagery,
+    leaflet::addProviderTiles(leaflet::providers$Esri.WorldImagery,
                      group = "Satellite") |>
-    addRasterImage(x,
+    leaflet::addRasterImage(x,
                    layerId = "Risk Map",
                    group = "Risk Map",
                    colors = mycolors) |>
-    addMiniMap(tiles = providers$Jawg.Light,
+    leaflet::addMiniMap(tiles = leaflet::providers$Jawg.Light,
                toggleDisplay = TRUE) |>
     leaflegend::addLegendFactor(values = values(x),
                                 pal = pal,
@@ -55,19 +67,22 @@ plot_map <- function(x, legend_title = NULL,
                                 position = "bottomleft",
                                 orientation = 'horizontal') |>
     leaflet.opacity::addOpacitySlider(layerId = "Risk Map") |>
-    leafem::addImageQuery(x, project = FALSE,
-                          type="mousemove",
+    leafem::addImageQuery(x,
+                          project = FALSE,
+                          type = "mousemove",
                           layerId = "Risk Map",
                           digits = 2
     ) |>
-    addLayersControl(position = "bottomright",
+    leaflet::addLayersControl(position = "bottomright",
                      baseGroups = c("Basemap", "Satellite"),
                      overlayGroups = c("Risk Map"),
-                     options = layersControlOptions(collapsed = TRUE))
+                     options = leaflet::layersControlOptions(collapsed = TRUE))
 
   # export html file
-  htmlwidgets::saveWidget(widget = outmap, file = path_out,
+  if (!is.null(path_out)) {
+    htmlwidgets::saveWidget(widget = outmap, file = path_out,
                           selfcontained = TRUE)
+  }
 
   return(outmap)
 
