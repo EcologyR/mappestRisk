5868859dbd8ef48501fb66c78f5c420fb455cc37
AMBarbosa <ana.marcia.barbosa@gmail.com>
2023-05-10 20:33:24 +0200

    added 'output' argument (binary or value)

--- R/map_risk.R
+++ R/map_risk.R
@@ -4,7 +4,9 @@
 #' right (maximum) temperature bounds for optimal performance of the target species.
 #' @param t_rast Optional 12-layer SpatRaster with monthly mean temperatures
 #' for (at least) the target 'region'. If not provided, global WorldClim raster
-#'  layers will be (down)loaded and cropped to 'region'.
+#' layers will be (down)loaded and cropped to 'region'. Note that the download
+#' can be slow the first time you use the function in a new working directory.
+#' If you get an error, consider running e.g. [options(timeout = 500)].
 #' @param region Optional object specifying the region to map.
 #' Can be a 'SpatVector' polygon map (obtained with [terra::vect()]);
 #' or a 'SpatExtent' object (obtained with [terra::ext()]),
@@ -14,36 +16,51 @@
 #' If NULL, the output maps will cover the entire `t_rast` if provided,
 #' or the entire world otherwise.
 #' @param res Argument to pass to [geodata::worldclim_global()] specifying
-#' the spatial resolution for the raster maps to download, if 't_rast' is not provided.
-#' The default is 2.5 arc-minutes.
-#' @param path Argument to pass to [geodata::worldclim_global()] (if `t_rast` is not provided)
-#' and/or to [geodata::world()] (if 'region' is a vector of country names)
-#' specifying the folder path for the downloaded maps.
-#' @param mask Logical value to pass to [terra::crop()] specifying whether
-#' the output raster maps should be masked with the borders of the target 'region',
+#' the spatial resolution for the raster maps to download, if 't_rast' is not
+#' provided. The default is 2.5 arc-minutes.
+#' @param path Argument to pass to [geodata::worldclim_global()] (if `t_rast`
+#' is not provided) and/or to [geodata::world()] (if 'region' is a vector of
+#'  country names) specifying the folder path for the downloaded maps.
+#' @param mask Logical value to pass to [terra::crop()] specifying whether the
+#' output raster maps should be masked with the borders of the target 'region',
 #' if this is a polygon map or a vector of country names. The default is TRUE.
 #' If FALSE, the entire rectangular extent of 'region' will be used.
-#'
-#' @return This function returns a SpatRaster object with 13 raster layers
-#' of temperature (one for each month, and a final one with the mean value across months),
-#' with values only within the bounds of the input 't_vals'.
+#' @param output Character value specifying the type of values in the output
+#' map pixels. Can be "binary" (the default), to identify pixels within (1)
+#' and outside (0) the bounds of the input 't_vals'; or "value", to assign
+#' to pixels the temperature values where these are within the bounds of the
+#' input 't_vals', and NA where they are not.
+#' @return This function returns a SpatRaster object with 13 layers: one for
+#' each month, and a final summary layer with the sum (if output="binary") or
+#' the mean (if output="value") across months.
 #' @export
-#'
 #' @examples
-#' risk_rasters <- map_risk(t_vals = c(12.5, 21.4), region = "Spain")
+#' risk_rasters <- map_risk(t_vals = c(12.5, 21.4), path = "downloaded_maps",
+#' region = c("Portugal", "Spain"))  # note that 't_vals' should be an output
+#' of fit_devmodels()
+#'
 #' terra::plot(risk_rasters)
+#'
 #' terra::plot(risk_rasters[[13]])
-#' # terra::writeRaster(risk_rasters[[13]], "risk_mean.tif")
+#'
+#' # if you want to save some map(s) to disk:
+#' \dontrun{
+#' terra::writeRaster(risk_rasters[[13]], "risk_map.tif")  # exported map can be seen with GIS software
+#' }
 #'
 map_risk <- function(t_vals = NULL,
                      t_rast = NULL,
                      region = NULL,
                      res = 2.5,
                      path = NULL,
-                     mask = TRUE
-                     ) {
+                     mask = TRUE,
+                     output = "binary"
+) {
 
-  stopifnot(is.numeric(t_vals), length(t_vals) == 2, t_vals[2] > t_vals[1])
+  stopifnot(is.numeric(t_vals),
+            length(t_vals) == 2  #,
+            # t_vals[2] > t_vals[1]
+  )
 
   if (!is.null(t_rast)) {
     stopifnot(inherits(t_rast, "SpatRaster"))
@@ -76,23 +93,27 @@ map_risk <- function(t_vals = NULL,
     region <- subset(wrld, wrld$NAME_0 %in% region)
   }
 
-  tavg <- t_rast     # Why not use t_rast directly? No need to duplicate object?
-
-  if (is.null(tavg)) {
-    tavg <- geodata::worldclim_global(var = "tavg",
-                                      res = res,
-                                      path = path)
+  if (is.null(t_rast)) {
+    t_rast <- geodata::worldclim_global(var = "tavg",
+                                        res = res,
+                                        path = path)
   }
 
-  tavg_crop <- terra::crop(tavg,
-                           region,
-                           mask = mask)
-
-  # add raster layer with mean across months:
-  tavg_crop <- c(tavg_crop, terra::app(tavg_crop, "mean", na.rm = TRUE))
+  t_rast_crop <- terra::crop(t_rast,
+                             region,
+                             mask = mask)
 
   # delete values outside thermal optimum zones:
-  tavg_crop[tavg_crop < min(t_vals) | tavg_crop > max(t_vals)] <- NA
+  outside <- t_rast_crop < min(t_vals) | t_rast_crop > max(t_vals)
+  t_rast_crop[outside] <- ifelse(output == "binary", 0, NA)
 
-  return(tavg_crop)
+  if (output == "binary") {
+    t_rast_crop[!outside] <- 1
+  }
+
+  # add 13th raster layer with sum or mean across months:
+  fun <- ifelse(output == "binary", "sum", "mean")
+  t_rast_crop <- c(t_rast_crop, terra::app(t_rast_crop, fun, na.rm = TRUE))
+
+  return(t_rast_crop)
 }
--- tests/test-map_risk.R
+++ tests/test-map_risk.R
@@ -6,7 +6,7 @@ ext_region <- terra::ext(-10, 10, 30, 40)
 
 # Test the function with default arguments:
 test_that("map_risk function works with default arguments", {
-  result <- map_risk(t_vals = t_vals)
+  result <- map_risk(t_vals = t_vals, path = "downloaded_maps")
   expect_true(is(result, "SpatRaster"))
   expect_equal(length(result), 13)
 })
@@ -15,7 +15,7 @@ test_that("map_risk function works with default arguments", {
 library(terra)
 region <- system.file("ex/lux.shp", package="terra")
 test_that("map_risk function works with a SpatVector region", {
-  result <- map_risk(t_vals = t_vals, region = region)
+  result <- map_risk(t_vals = t_vals, region = region, path = "downloaded_maps")
   expect_true(is(result, "SpatRaster"))
   expect_equal(length(result), 13)
 })
@@ -23,7 +23,7 @@ test_that("map_risk function works with a SpatVector region", {
 # Test the function with a SpatExtent region:
 extent <- ext(-10, 10, 30, 50)
 test_that("map_risk function works with a SpatExtent region", {
-  result <- map_risk(t_vals = t_vals, region = extent)
+  result <- map_risk(t_vals = t_vals, region = extent, path = "downloaded_maps")
   expect_true(is(result, "SpatRaster"))
   expect_equal(length(result), 13)
 })
@@ -39,7 +39,7 @@ test_that("map_risk function works with a numeric vector region", {
 # Test the function with a character region:
 region <- "Luxembourg"
 test_that("map_risk function works with a character vector region", {
-  result <- map_risk(t_vals = t_vals, region = region)
+  result <- map_risk(t_vals = t_vals, region = region, path = "downloaded_maps")
   expect_true(is(result, "SpatRaster"))
   expect_equal(length(result), 13)
 })
@@ -47,43 +47,43 @@ test_that("map_risk function works with a character vector region", {
 # Test the function with multiple character regions:
 test_that("map_risk works for multiple character regions", {
   region <- c("Luxembourg", "Belgium")
-  result <- map_risk(t_vals = t_vals, region = region)
+  result <- map_risk(t_vals = t_vals, region = region, path = "downloaded_maps")
   expect_equal(class(result), "SpatRaster")
   expect_equal(nlyr(result), 13)
 })
 
 # Test that the values outside the temperature bounds are NA
 test_that("values outside temperature bounds are NA", {
-  result <- map_risk(t_vals = t_vals, region = "Switzerland")
+  result <- map_risk(t_vals = t_vals, region = "Switzerland", path = "downloaded_maps")
   expect_true(all(is.na(result[[1]][result[[1]] < 12.5 | result[[1]] > 21.4])))
 })
 
 # Test that the function returns the same result when using a SpatExtent or a SpatVector as input for region
 test_that("the function returns the same result with SpatExtent and SpatVector input for region", {
   vec_region <- terra::vect(ext_region)
-  result1 <- map_risk(t_vals = t_vals, region = ext_region)
-  result2 <- map_risk(t_vals = t_vals, region = vec_region)
+  result1 <- map_risk(t_vals = t_vals, region = ext_region, path = "downloaded_maps")
+  result2 <- map_risk(t_vals = t_vals, region = vec_region, path = "downloaded_maps")
   expect_identical(result1, result2)
 })
 
 # Test that the function returns an error when providing an invalid numeric vector for t_vals
 test_that("the function returns an error when providing an invalid numeric vector for region", {
-  expect_error(map_risk(t_vals = t_vals[1], region = c(-10, 10, 30, 40)))
+  expect_error(map_risk(t_vals = t_vals[1], region = c(-10, 10, 30, 40), path = "downloaded_maps"))
 })
 
 # Test that the function returns an error when providing an invalid numeric vector for region
 test_that("the function returns an error when providing an invalid numeric vector for region", {
-  expect_error(map_risk(t_vals = t_vals, region = c(-10, 10, 30)))
+  expect_error(map_risk(t_vals = t_vals, region = c(-10, 10, 30), path = "downloaded_maps"))
 })
 
 # Test that the function returns an error when providing an invalid character vector for region
 test_that("the function returns an error when providing an invalid character vector for region", {
-  expect_error(map_risk(t_vals = t_vals, region = "nowhere"))
+  expect_error(map_risk(t_vals = t_vals, region = "nowhere", path = "downloaded_maps"))
 })
 
 # Test that the output of the function is cropped to the extent of the input region
 test_that("the output is cropped to the extent of the input region", {
-  result <- map_risk(t_vals = t_vals, region = ext_region)
+  result <- map_risk(t_vals = t_vals, region = ext_region, path = "downloaded_maps")
   expect_equal(ext(result), ext_region)
 })
 
@@ -91,16 +91,40 @@ test_that("the output is cropped to the extent of the input region", {
 test_that("map_risk returns same result for t_rast and worldclim_global()", {
 
   # Create SpatRaster with worldclim_global()
-  wclim <- geodata::worldclim_global(var = "tavg", res = 10)
+  wclim <- geodata::worldclim_global(var = "tavg", res = 10, path = "downloaded_maps")
   t_rast <- terra::crop(wclim, region)
 
   # Run function with t_rast
-  result_t_rast <- map_risk(t_vals = t_vals, t_rast = t_rast)
+  result_t_rast <- map_risk(t_vals = t_vals, t_rast = t_rast, path = "downloaded_maps")
 
   # Run function with worldclim_global()
-  result_worldclim <- map_risk(t_vals = t_vals, region = region, res = res)
+  result_worldclim <- map_risk(t_vals = t_vals, region = region, res = res, path = "downloaded_maps")
 
   # Test output
   expect_identical(result_t_rast, result_worldclim)
 
+})
+
+# Binary output, with temperature range covering the entire range of the data, should produce only 0s and 1s
+test_that("Binary output produces SpatRaster with pixel values of 0 or 1", {
+  r <- map_risk(t_vals = c(-50, 50), output = "binary")
+  expect_true(all(r[] %in% c(0, 1), na.rm = TRUE))
+})
+
+# Value output, with temperature range covering the entire range of the data, should produce pixel values between the min and max temperature
+test_that("Value output produces SpatRaster with pixel values between min and max temperature", {
+  r <- map_risk(t_vals = c(-50, 50), output = "value")
+  expect_true(all(r[] >= -50 & r[] <= 50, na.rm = TRUE))
+})
+
+# Binary output, with temperature range outside the range of the data, should produce only 0s
+test_that("Binary output with temperature range outside data range produces all 0s", {
+  r <- map_risk(t_vals = c(-100, -50), output = "binary")
+  expect_true(all(r[] == 0, na.rm = TRUE))
+})
+
+# Value output, with temperature range outside the range of the data, should produce only NAs
+test_that("Value output with temperature range outside data range produces all NAs", {
+  r <- map_risk(t_vals = c(-100, -50), output = "value")
+  expect_true(all(is.na(r[])))
 })
