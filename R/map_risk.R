c617fd891045723e0e16322e52008889d16999ee
AMBarbosa <ana.marcia.barbosa@gmail.com>
2023-05-11 13:15:22 +0200

    add example temperature file; fix map_risk tests

--- R/map_risk.R
+++ R/map_risk.R
@@ -1,26 +1,24 @@
 #' Map pest risk
 #'
 #' @param t_vals Numeric vector of length 2 specifying the left (minimum) and
-#' right (maximum) temperature bounds for optimal performance of the target species.
-#' @param t_rast Optional 12-layer SpatRaster with monthly mean temperatures
+#' right (maximum) temperature bounds for optimal performance of the target
+#' species.
+#' @param t_rast Optional 12-layer 'SpatRaster' with monthly mean temperatures
 #' for (at least) the target 'region'. If not provided, global WorldClim raster
 #' layers will be (down)loaded and cropped to 'region'. Note that the download
 #' can be slow the first time you use the function in a new working directory.
 #' If you get an error, consider running e.g. [options(timeout = 500)].
-#' @param region Optional object specifying the region to map.
-#' Can be a 'SpatVector' polygon map (obtained with [terra::vect()]);
-#' or a 'SpatExtent' object (obtained with [terra::ext()]),
-#' or a numeric vector of length 4 specifying the region coordinates
-#' in the order xmin, xmax, ymin, ymax;
-#' or a character vector of country name(s) in English.
-#' If NULL, the output maps will cover the entire `t_rast` if provided,
-#' or the entire world otherwise.
+#' @param region Optional object specifying the region to map. Can be a
+#' 'SpatVector' polygon map (obtained with [terra::vect()]); or a 'SpatExtent'
+#' object (obtained with [terra::ext()]); or a numeric vector of length 4
+#' specifying the region coordinates in the order xmin, xmax, ymin, ymax;
+#' or a character vector of country name(s) in English. If NULL, the output maps #' will cover the entire `t_rast` if provided, or the entire world otherwise.
 #' @param res Argument to pass to [geodata::worldclim_global()] specifying
 #' the spatial resolution for the raster maps to download, if 't_rast' is not
 #' provided. The default is 2.5 arc-minutes.
 #' @param path Argument to pass to [geodata::worldclim_global()] (if `t_rast`
 #' is not provided) and/or to [geodata::world()] (if 'region' is a vector of
-#'  country names) specifying the folder path for the downloaded maps.
+#' country names) specifying the folder path for the downloaded maps.
 #' @param mask Logical value to pass to [terra::crop()] specifying whether the
 #' output raster maps should be masked with the borders of the target 'region',
 #' if this is a polygon map or a vector of country names. The default is TRUE.
@@ -30,22 +28,40 @@
 #' and outside (0) the bounds of the input 't_vals'; or "value", to assign
 #' to pixels the temperature values where these are within the bounds of the
 #' input 't_vals', and NA where they are not.
-#' @return This function returns a SpatRaster object with 13 layers: one for
+#' @return This function returns a 'SpatRaster' object with 13 layers: one for
 #' each month, and a final summary layer with the sum (if output="binary") or
 #' the mean (if output="value") across months.
 #' @export
 #' @examples
-#' risk_rasters <- map_risk(t_vals = c(12.5, 21.4), path = "downloaded_maps",
-#' region = c("Portugal", "Spain"))  # note that 't_vals' should be an output
-#' of fit_devmodels()
+#' tavg_file <- system.file("extdata/tavg_lux.tif", package = "mappestRisk")
+#' tavg_rast <- terra::rast(tavg_file)
+#' terra:::plot(tavg_rast)
 #'
-#' terra::plot(risk_rasters)
+#' risk_rast_binary <- map_risk(t_vals = c(12.5, 21.4), t_rast = tavg_rast)
+#' # note that 't_vals' should be an output of fit_devmodels()
 #'
-#' terra::plot(risk_rasters[[13]])
+#' terra::plot(risk_rast_binary)
 #'
-#' # if you want to save some map(s) to disk:
+#' terra::plot(risk_rast_binary[[13]])
+#'
+#'
+#' risk_rast_value <- map_risk(t_vals = c(12.5, 21.4), t_rast = tavg_rast,
+#' output = "value")
+#'
+#' terra::plot(risk_rast_value)
+#'
+#' terra::plot(risk_rast_value[[13]])
+#'
 #' \dontrun{
-#' terra::writeRaster(risk_rasters[[13]], "risk_map.tif")  # exported map can be seen with GIS software
+#' # if you don't have temperature rasters for your region:
+#' risk_rast <- map_risk(t_vals = c(12.5, 21.4), path = "downloaded_maps",
+#' region = c("Portugal", "Spain"))
+
+#' terra::plot(risk_rast[[13]])
+
+#' # if you want to save some output map(s) to disk:
+#' terra::writeRaster(risk_rast[[13]], "risk_map.tif")  # exported maps can
+#' be seen with GIS software
 #' }
 #'
 map_risk <- function(t_vals = NULL,
@@ -59,7 +75,7 @@ map_risk <- function(t_vals = NULL,
 
   stopifnot(is.numeric(t_vals),
             length(t_vals) == 2  #,
-            # t_vals[2] > t_vals[1]
+            # t_vals[2] > t_vals[1]  # not necessary
   )
 
   if (!is.null(t_rast)) {
@@ -68,19 +84,21 @@ map_risk <- function(t_vals = NULL,
   }
 
   if (is.null(t_rast) & is.null(path)) {
-    stop("Please provide a path to download the temperature rasters.")
+    stop("Please provide a 'path' to save the downloaded temperature maps.")
   }
 
   if (is.null(region)) {
     region <- terra::ext()  # whole world
-    mask <- FALSE  # pointless otherwise
   }
 
   if (is.numeric(region)) {
-    if (length(region != 4)) {
+    if (length(region) != 4) {
       stop("When provided as a numeric vector, 'region' must have length 4 (xmin, xmax, ymin, ymax).")
     }
     region <- terra::ext(region)
+  }
+
+  if (is(region, "SpatExtent")) {
     mask <- FALSE  # pointless otherwise
   }
 
@@ -105,7 +123,7 @@ map_risk <- function(t_vals = NULL,
 
   # delete values outside thermal optimum zones:
   outside <- t_rast_crop < min(t_vals) | t_rast_crop > max(t_vals)
-  t_rast_crop[outside] <- ifelse(output == "binary", 0, NA)
+  t_rast_crop[outside] <- ifelse(output == "binary", 0, NA_real_)
 
   if (output == "binary") {
     t_rast_crop[!outside] <- 1
