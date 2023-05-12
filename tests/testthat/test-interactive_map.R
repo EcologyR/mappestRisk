f4b56318328fd519c9a097b93f69d5e908f89fa6
ajpelu <ajpelu@gmail.com>
2023-05-12 13:40:49 +0200

    remove garbage

--- tests/testthat/test-interactive_map.R
+++ tests/testthat/test-interactive_map.R
@@ -11,15 +11,3 @@ r <- risk_rast_binary[[13]]
 test_that("interactive_map throws an error if x is not a SpatRaster", {
   expect_error(interactive_map(x = "not a raster"))
 })
-
-test_that("interactive_map returns a leaflet object", {
-  # Test that the function returns a leaflet map object
-  expect_s3_class(interactive_map(x = r), "leaflet")
-  })
-
-grepl("EPSG", crs(x))
-
-grepl(crs(x), "EPSG")
-
-
-crs(x, warn = TRUE)
