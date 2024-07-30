1395d32769374344438a06054f020ada1bbd014e
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-07-30 09:43:55 +0200

    revised datasets

--- R/datasets.R
+++ R/datasets.R
@@ -1,13 +1,50 @@
+#' <b><i>Brachycaudus schwartzi</i> whole life cycle development rate across temperatures</b>
+#'
+#' A modified data set from Table 1 in Satar and Yokomi (2002) on days of development
+#' for *Brachycaudus schwartzi* across different constant temperatures and life stages
+#'
+#' @name aphid
+#' @usage data(aphid)
+#' @format ## `aphid`
+#' A data frame with 7 rows and 5 columns.
+#'    The workflow is reproducible and available in `/data-raw` folder of the <a href="https://github.com/EcologyR/mappestRisk">mappestRisk GitHub repository</a>,
+#'    which includes both the original summarized data set -`satar_data.xlsx`- and the
+#'    R script with the dev. days to dev. rate conversion in `prepare_aphid.R`.
+#'
+#'
+#' \describe{
+#'   \item{reference}{"Satar2002" refers to the source paper as cited below in section `Source`.}
+#'   \item{temperature}{Temperature treatments (ºC).}
+#'   \item{dev_days}{Development days (i.e., days to fulfill development requirements from a life-stage to the following)}
+#'   \item{rate_value}{Rate of Development (1/days), the reciprocal of Development days, see `dev_days`}
+#'   \item{stage}{Life stage or instar evaluated. In this case, only data of the whole immature stages (i.e., nymphs) were used}
+#' }
+#' @source {<b>Satar, S. and Yokomi, R. (2002)</b>. Effect of temperature and host on development of
+#'  <i>Brachycaudus schwartzi</i> (Homoptera: Aphididae).
+#'   Ann. Entomol. Soc. Am. 95: 597-602. <https://doi.org/10.1603/0013-8746(2002)095[0597:EOTAHO]2.0.CO;2>}.</br>
+#'
+#'   Licence: <a href="https://creativecommons.org/licenses/by-nc/3.0">CC BY-NC 3.0</a> (modified material).
+"aphid"
+
+
+
+#' Country names
+#'
+#' @format ## `country_names`
+#' A character vector of country names (length = 231 countries)
+#' @source https://gadm.org
+"country_names"
+
+
 #' <b>Available Models Table</b>
 #'
-#' This data.frame, called `dev_model_table` in your environment once the package is loaded,
+#' This data.frame, called `available_models` in your environment once the package is loaded,
 #' contains the available models to fit using [fit_devmodels()]. It is useful for the user to know it
 #' before applying functions in the package.
 #'
 #' @name available_models
-#' @docType data
 #' @usage data("available_models")
-#' @format ## `dev_model_table`
+#' @format ## `available_models`
 #' A data.frame/tibble with 13 rows and 6 columns. This package contain the equation names of the models that are
 #' able to be fitted with [fit_devmodels()] and the equivalent names in their respective
 #' source packages underlying the start value exploration function and formulas used in-code to fit models.
@@ -34,5 +71,4 @@
 #'
 #'
 #'
-"dev_model_table"
-
+"available_models"
