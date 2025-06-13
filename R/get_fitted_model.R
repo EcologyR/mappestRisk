45389ead74dd0ae3984e0098b5f09d4047144376
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2025-06-13 19:04:00 +0200

    Add fun to get fitted model #80

---
+++ R/get_fitted_model.R
@@ -0,0 +1,27 @@
+#' Get fitted model object
+#'
+#' @param fitted_df A table with fitted models, as produced by [fit_devmodels()].
+#' @param model_name Character. Name of a fitted model, see [available_models].
+#'
+#' @returns A model object
+#' @export
+#'
+#' @examples
+#' data("aphid")
+#'
+#' fitted_tpcs_aphid <- fit_devmodels(temp = aphid$temperature,
+#'                                    dev_rate = aphid$rate_value,
+#'                                    model_name = c("lactin2", "briere2", "mod_weibull")
+#'                                    )
+#' get_fitted_model(fitted_tpcs_aphid, "briere2")
+get_fitted_model <- function(fitted_df = NULL, model_name = NULL) {
+
+  stopifnot(inherits(df, "data.frame"))
+  stopifnot(is.character(model_name) & length(model_name) == 1)
+  if (!model_name %in% unique(df$model_name)) {
+    stop(model_name, " does not appear in the table of fitted models.")
+  }
+
+  df$model_fit[df$model_name == model_name][[1]]
+
+}
