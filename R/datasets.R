be0f1b85d835ed13fdb4fcf7cf8ce69ad6f69036
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2025-06-13 16:27:27 +0200

    revise data docs

--- R/datasets.R
+++ R/datasets.R
@@ -38,31 +38,38 @@
 
 #' Available Models Table
 #'
-#' This data.frame, called `available_models` in your environment once the package is loaded,
-#' contains the available models to fit using [fit_devmodels()]. It is useful for the user to know it
-#' before applying functions in the package.
+#' Table containing the available models to be fit using [fit_devmodels()].
+#' These models come from two other packages:
+#' <a href="https://cran.r-project.org/package=devRate">`devRate` </a> and
+#' <a href="https://padpadpadpad.github.io/rTPC/">`rTPC` </a>.
 #'
 #' @name available_models
 #' @usage data("available_models")
 #' @format ## `available_models`
-#' A data.frame/tibble with 13 rows and 6 columns. This package contain the equation names of the models that are
-#' able to be fitted with [fit_devmodels()] and the equivalent names in their respective
-#' source packages underlying the start value exploration function and formulas used in-code to fit models.
-#' These models come from two packages that are already published:
-#'  <a href="https://github.com/frareb/devRate">`devRate` </a> and <a href="https://github.com/padpadpadpad/rTPC">`rTPC` </a>. </br>
+#' A data.frame/tibble with 13 rows and 6 columns:
 #'
 #' \describe{
-#'   \item{model_name}{This is the name of the model equations that the user can use as input in the package functions.}
-#'   \item{package}{names of the packages used by [fit_devmodels()] to obtain appropriate start values for the user-providen data.
-#'    When the package is <a href="https://github.com/padpadpadpad/rTPC">rTPC package</a>, start values are
-#'    automatically computed with [rTPC::get_start_vals()], which in turn relies on [nls.multstart::nls_multstart()]. When the package is  <a href="https://github.com/frareb/devRate">devRate package</a>,
-#'    iterative starting values are computed using [nls.multstart::nls_multstart()], using the parameters published in
-#'    [devRate::devRateEqStartVal()] as first attempts to iterate. As an exception, if `model_name == "briere1"`,
-#'    generic starting values are provided and advised to the user due to unrealistic value of some parameters in the
+#'
+#'   \item{model_name}{Model name to be used within [fit_devmodels()].}
+#'
+#'   \item{package}{names of the packages used by [fit_devmodels()] to obtain
+#'   appropriate start values for the user-provided data.
+#'    When the package is <a href="https://padpadpadpad.github.io/rTPC/">rTPC package</a>,
+#'    start values are automatically computed with [rTPC::get_start_vals()],
+#'    which in turn relies on [nls.multstart::nls_multstart()].
+#'    When the package is <a href="https://github.com/frareb/devRate">devRate package</a>,
+#'    iterative starting values are computed using [nls.multstart::nls_multstart()],
+#'    using the parameters published in [devRate::devRateEqStartVal()]
+#'    as first attempts to iterate.
+#'    As an exception, if `model_name == "briere1"`, generic starting values are provided
+#'    and advised to the user due to the unrealistic value of some parameters in the
 #'    [devRate] data set.}
-#'   \item{source_model_name}{function name in source packages used in the background of this package}
-#'   \item{formula, working formula, params_formula}{formulas allowing automation of model fitting, with no information of interest for the user}
+#'
+#'   \item{source_model_name}{name of the function in the source packages `rTPC` and `devRate`.}
+#'
+#'   \item{formula, working_formula, n_params}{formulas used for model fitting.}
 #' }
+#'
 #' @source {<b>Rebaudo, F., Struelens, Q., and Dangles, O. (2018)</b>. Modelling temperature-dependent development rate and phenology in
 #' arthropods: The `devRate` package for r. Methods Ecol Evol. 9: 1144-1150. <https://doi.org/10.1111/2041-210X.12935>
 #'
